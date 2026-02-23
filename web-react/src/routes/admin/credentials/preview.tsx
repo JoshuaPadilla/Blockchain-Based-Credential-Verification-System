63420931269a5ad50b2549d000b19fb6c38bc23c
MrWang <joshuapadilla706@gmail.com>
2026-02-24 06:04:04 +0800

    feat: add credential preview page and fonts

---
+++ web-react/src/routes/admin/credentials/preview.tsx
@@ -0,0 +1,17 @@
+import { DiplomaFinal } from "@/components/pdf_documents/diploma_final/diploma_final";
+import { PDFViewer } from "@react-pdf/renderer";
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/admin/credentials/preview")({
+	component: RouteComponent,
+});
+
+function RouteComponent() {
+	return (
+		<div className="flex flex-col h-screen w-full">
+			<PDFViewer className="w-full h-full border-none">
+				<DiplomaFinal />
+			</PDFViewer>
+		</div>
+	);
+}
